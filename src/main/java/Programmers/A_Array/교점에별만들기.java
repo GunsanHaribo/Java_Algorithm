02a4c27d36a6bef5b462a904908826b083826d95
GunsanHaribo <hth99131@gmail.com>
2024-03-22 18:59:08 +0900

    fail_crossPoint

--- src/main/java/Programmers/A_Array/교점에별만들기.java
+++ src/main/java/Programmers/A_Array/교점에별만들기.java
@@ -1,100 +1,151 @@
 package Programmers.A_Array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.Math.abs;
 
 public class 교점에별만들기 {
 
 
     public static void main(String[] args) {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-
         // 배열에 배열 넣어서 만든다
         int[][] lines = new int[5][5];
 
-        lines[0] = new int[]{2, -1, 4};
-        lines[1] = new int[]{-2, -1, 4};
-        lines[2] = new int[]{0, -1, 1};
-        lines[3] = new int[]{5, -8, -12};
-        lines[4] = new int[]{5, 8, 12};
+//        lines[0] = new int[]{2, -1, 4};
+//        lines[1] = new int[]{-2, -1, 4};
+//        lines[2] = new int[]{0, -1, 1};
+//        lines[3] = new int[]{5, -8, -12};
+//        lines[4] = new int[]{5, 8, 12};
 
-//        lines[0] = new int[]{0, 1, -1};
-//        lines[1] = new int[]{1, 0, -1};
-//        lines[2] = new int[]{1, 0, 1};
+        // 여기서 틀린다
+        lines[0] = new int[]{0, 1, -1};
+        lines[1] = new int[]{1, 0, -1};
+        lines[2] = new int[]{1, 0, 1};
 
 //        lines[0] = new int[]{1, -1, 0};
 //        lines[1] = new int[]{2, 1, 0};
+        List<int[]> crossPoint = getCrossPoint(lines);
+
+//        System.out.println(Arrays.deepToString(crossPoint));
+        for(int[] i : crossPoint){
+            System.out.println("x좌표: " + i[0] + " y좌표: "+ i[1]);
+        }
+        // 교점 구하기 완료
+
+        // fixNumericAmount
+        // 최대 좌표에서 나누기 2를 하고  x좌표 = x+ max, y좌표 =  max  - (y)
+        //이 max가 근데 절댓값 max인것 같은데;
+        int max = 0;
+        for(int[] i : crossPoint){
+            for(int j : i){
+                if(max < abs(j)){
+                    max = abs(j);
+                }
+            }
+        }
+        System.out.println(max);
+
+        // 좌표 구성  //[*.*, ..., ...]
+        // 여기서 좌표 만들떄 범위를 좀.. 잘설정해야 되는데...다음 값이 없으면 좀 숫자르 날린다 던가...
+        String[][] result = new String[2*max+1][2*max+1]; // 전체 배열 초기화 해야되는데
+        //
+
+        for(String[] st : result){
+            Arrays.fill(st, ".");
+        }
+
+        for(int[] i : crossPoint){
+            int trans_x = i[0]+ max;
+            int trans_y = max - i[1] ;
+            result[trans_y][trans_x] = "*";
+        }
+
+        System.out.println(Arrays.deepToString(result));
+
+        String[] finalResult = new String[result.length];
+
+        StringBuilder sb;
+        for(int i  = 0; i< finalResult.length; i++){
+            sb = new StringBuilder();
+            for(int j = 0; j< finalResult.length; j++){
+                sb.append(result[i][j]);
+            }
+            finalResult[i] = sb.toString();
+        }
+
+        System.out.println(Arrays.deepToString(finalResult));
+
+    }
+    // fixNumericAmount
 
 
 
-//        System.out.println(lines.length);
-        List<int[]> answer = new ArrayList<>();
-        for(int i = 0; i< lines.length; i++){
-            for(int j = i+1; j< lines.length; j++){
 
-                if(calculate_dot(lines[i], lines[j]) != null){
-                    answer.add(calculate_dot(lines[i], lines[j]));
+    // 함수를 만들어서 교점 추출 리스트반환
+    public static List<int[]>  getCrossPoint(int[][] lines){
+
+        List<int[]> result = new ArrayList<>();
+
+        for(int nowline=0; nowline< lines.length-1; nowline++){
+            for(int nextLine = nowline+1; nextLine < lines.length; nextLine++){
+                // 교점을 구한다
+                int A = lines[nowline][0];
+                int B = lines[nowline][1];
+                int E = lines[nowline][2];
+
+                int C = lines[nextLine][0];
+                int D = lines[nextLine][1];
+                int F = lines[nextLine][2];
+
+                float low = (A*D- B*C);
+                float x_top = B*F - E*D;
+                float y_top = E*C - A*F;
+
+                float[] floatPoint  = new float[2];
+                int[] finalPoint = new int[2];
+                int x =0;
+                int y = 1;
+                if(low == 0){
+                    continue;
+                }else {
+                    floatPoint[x] = x_top / low;
+                    floatPoint[y] = y_top / low;
+                    boolean checkInt = checkPointInt(floatPoint[x], floatPoint[y]);
+                    if(checkInt){
+                        finalPoint[x] = (int) floatPoint[x];
+                        finalPoint[y] = (int) floatPoint[y];
+
+                        result.add(finalPoint);
+                    }
+
                 }
 
             }
         }
 
-
-        for(int[] m : answer){
-            System.out.println("처음 : "+m[0]+ ", 다음 :"+m[1]);
-        }
-
-        // 최대 값 식별, String 배열로 좌표 만들고 거기에 해당하는 값만 또는
-        // 이 값들에 특정 값을 더해서 좌표 형태로 몇개 만들어준다
-        //좌표 형태로 안바뀌는데...? 좀더 고민 해봐야 할듯//좌표를 어떻게 문자열로 할까
-
-
-        //최대 최솟값 구해서 2차원 배열 만든다
-
-
-
-
-
-
-
-
+        return result;
     }
 
+    // 의문 1. static 이거 밖에하면 안되는이유 찾아야됩니다
+    // 의문 cs. 만약에 int float이랑 크기 차이가 나면 => 근데 아마 1.0 이랑 1이랑 cs 때문에 다르게 찍힐 것 같은데
+    public static boolean checkPointInt(float x , float y){
+        boolean check = true;
 
+        int x1 = (int) x;
 
-    public static int[] calculate_dot(int[] first, int[] second){
+        int y1 = (int) y;
 
-        if(first.length != 3  || second.length != 3){
-            throw new RuntimeException();
+        if(x != x1 || y1 != y){
+            check = false;
         }
 
-        int x = 0;
-        int y = 0;
-
-        try{
-            float x1 = (float) (first[1] * second[2] - first[2] * second[1]) /(first[0]*second[1] - first[1]*second[0]);
-            float y1 = (float) (first[2] * second[0] - first[0] * second[2]) /(first[0]*second[1] - first[1]*second[0]);
-
-
-            // 만약에 x,y가 정수 값이 아니면 담지 않는
-            //분모 값이 0이거나 그래서 나눈 값이 float 일떄
-            if((x1 == (int) x1) && (y1 == (int) y1) ){
-                x = (int) x1;
-                y= (int) y1;
-            }else{
-                return null;
-            }
-
-
-        }catch (Exception e){
-            System.out.println(e+"다시해");
-            return null;
-        }
-
-        return new int[]{x,y};
+        return check;
     }
 
 }
+
+
+
 
